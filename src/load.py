ab785021291ac1e5b153b7281aac0649b9bfb8bc
leorb99 <leo.rb1999@gmail.com>
2026-03-24 14:29:52 -0300

    q4 pronta

--- src/load.py
+++ src/load.py
@@ -9,13 +9,14 @@ line = {}
 lines = []
 columns = ['product_id', 'product_name', 'category', 'start_date', 'usd_price']
 for elem in import_costs:
-    line['product_id'] = elem['product_id']
-    line['product_name'] = elem['product_name']
-    line['category'] = elem['category']
-    line['start_date'] = elem['historic_data'][0]['start_date']
-    line['usd_price'] = elem['historic_data'][0]['usd_price']
-    lines.append(line.copy())
-
+    for i in range(len(elem['historic_data'])):
+        line['product_id'] = elem['product_id']
+        line['product_name'] = elem['product_name']
+        line['category'] = elem['category']
+        line['start_date'] = elem['historic_data'][i]['start_date']
+        line['usd_price'] = elem['historic_data'][i]['usd_price']
+        lines.append(line.copy())
+        
 df = pd.DataFrame(lines)
 df['start_date'] = pd.to_datetime(df['start_date'], dayfirst=True)
 df.to_csv('datasets/custos_importacao.csv', sep=',', index=False,)
